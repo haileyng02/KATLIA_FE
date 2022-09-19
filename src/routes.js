e56a44acd623d5fbd664dfc277341b83da42f4be
haileyng02 <haileynguyenss@gmail.com>
2022-09-19 15:27:16 +0700

    routes

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Home from './pages/Home'
+
+//Public routes
+const publicRoutes = [
+    {path: '/', component: Home}
+];
+
+//Private routes
+const privateRoutes = [];
+
+export {publicRoutes,privateRoutes}
