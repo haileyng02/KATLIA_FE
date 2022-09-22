37748951437e7c9a3b040f3087b2535688c2574c
haileyng02 <haileynguyenss@gmail.com>
2022-09-23 01:28:08 +0700

    Sale

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import mainPic1 from '../images/main-1.png'
 import mainPic2 from '../images/main-2.png'
 import mainArrow from '../images/shop-now-arrow.svg'
+import salePic from '../images/home-sale.png'
 
 const Home = () => {
   return (
-    <div className='px-[105px] py-[92px]'>
+    <div className='py-[92px]'>
       {/* Main */}
-      <div className=' h-[763px] relative'>
+      <div className='px-[105px] h-[763px] relative'>
         {/* Title */}
-        <div className='absolute left-[100px]'>
+        <div className='absolute left-[205px]'>
           <div className='flex gap-[73px]'>
             <div className='main-title'>
               <h1>KATLI</h1>
@@ -41,7 +42,19 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <span className=' border-t-1 border-black absolute right-0 w-[46.6%]'/>
+      <span className='black-line right-0 w-[46.6%]'/>
+      {/* Sale */}
+      <div className='mt-[101px] w-1/2 mx-auto relative'>
+        <img src={salePic} alt='Sale' className=' w-full'/>
+        <div className='absolute w-full h-full top-0 left-0 flex'>
+          <div className='m-auto text-20 leading-[25px]'>
+            <p className=' max-w-[350px] text-center'>The hot sale period has now begun. Don’t miss the news</p>
+            <div className='w-[220px] h-20 mx-auto text-center items-center flex justify-center mt-11 border-1 border-black'>
+              SEE THE SALE
+            </div>
+          </div>         
+        </div>
+      </div>
     </div>
   )
 }
