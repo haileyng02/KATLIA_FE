92eea1d3bd89097287303f76d6b1a8a934f15fd5
haileyng02 <haileynguyenss@gmail.com>
2022-10-02 16:14:12 +0700

    menu done raw

--- src/components/ProductThumbnail.jsx
+++ src/components/ProductThumbnail.jsx
@@ -5,7 +5,7 @@ const ProductThumbnail = ({item}) => {
   return (
     <Link to={`/product/${item.id}`} state={item}>
       <div>
-          <img src={item.image} alt='Item' className='aspect-[75/113]'/>
+          <img src={item.image} alt='Item' className='aspect-[2/3] object-cover object-center'/>
           <div className='mt-[23px] leading-6'>
               {item.colors===1||<p>{'+'+(item.colors-1)+' COLOURS'}</p>}
               <h2>{item.name}</h2>
