58e85a356f0b354330c5d983dbd546a7bb069d69
haileyng02 <haileynguyenss@gmail.com>
2022-11-21 16:26:22 +0700

    added skeleton to product detail

--- src/components/SimilarItems.jsx
+++ src/components/SimilarItems.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import ProductThumbnail from "../components/ProductThumbnail";
 import * as routes from "../api/apiRoutes";
 import appApi from "../api/appApi";
+import { Skeleton } from "antd";
 
-const SimilarItems = ({id}) => {
+const SimilarItems = ({ id }) => {
   const [items, setItems] = useState();
 
   //Get 4 Similar Items
@@ -27,20 +28,30 @@ const SimilarItems = ({id}) => {
   };
 
   useEffect(() => {
+    if (id == null) return;
     get4SimilarItems();
-  }, []);
+  }, [id]);
 
   return (
     <div className="mt-[141px] px-[150px]">
-      <h2
-        className=" text-[35px] leading-[44px] font-bold"
-      >
-        SIMILAR ITEMS
-      </h2>
+      <h2 className=" text-[35px] leading-[44px] font-bold">SIMILAR ITEMS</h2>
       <div className="mt-[47px] grid grid-flow-col auto-cols-[29%] gap-x-[86px] overflow-x-auto pb-[53px]">
-        {items?.map((item, i) => {
-          return <ProductThumbnail item={item} key={i} />;
-        })}
+        {items
+          ? items.map((item, i) => {
+              return <ProductThumbnail item={item} key={i} />;
+            })
+          : Array(3)
+              .fill()
+              .map((_, i) => (
+                <div key={i}>
+                  <Skeleton.Image
+                    style={{ width: "100%", height: "100%" }}
+                    active={true}
+                    className="aspect-[2/3] w-full"
+                  />
+                  <Skeleton className="mt-8" />
+                </div>
+              ))}
       </div>
     </div>
   );
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { Skeleton, Spin } from "antd";
 import Quantity from "../components/Quantity";
 import ColorIcon from "../components/ColorIcon";
 import SimilarItems from "../components/SimilarItems";
-import CartIcon from "../images/Cart2.svg";
 import * as routes from "../api/apiRoutes";
 import appApi from "../api/appApi";
-import { useEffect } from "react";
+import CartIcon from "../images/Cart2.svg";
+import loadingIcon from "../images/loading2.gif";
 
 const ProductDetail = () => {
   const location = useLocation();
@@ -61,87 +62,110 @@ const ProductDetail = () => {
     <div className=" pt-[71px] font-inter">
       <div className="px-[150px] flex items-start justify-between">
         {/* Left */}
-        <div className="flex basis-[47%] justify-between gap-x-7 items-start">
-          {/* Main Image */}
-          <img
-            src={currImage?.url}
-            alt="Main"
-            className="object-cover object-center basis-[85%] bg-gray-400"
-            style={{ height: "80vh" }}
-          />
-          {/* Sub Images */}
-          <div
-            className="grid grid-flow-row basis-[12.5%] gap-y-[10px] cursor-pointer overflow-y-auto"
-            style={{ height: currentColor?.imgList.length > 5 && "80vh" }}
-          >
-            {currentColor?.imgList.map((image, i) => {
-              return (
-                <img
-                  key={i}
-                  src={image.url}
-                  alt="Product"
-                  className={`object-cover object-center aspect-[55/82] hover:border-2 hover:border-primary hover:border-solid ${
-                    currImage.id === i && "border-2 border-primary border-solid"
-                  }`}
-                  onClick={() => imageOnClick(i, image.url)}
-                />
-              );
-            })}
-          </div>
-        </div>
-        {/* Right */}
-        <div className=" basis-[36.5%] mt-[82px]">
-          <h1 className=" font-bold">{item?.name}</h1>
-          <p className=" leading-6 mt-7">{item?.description}</p>
-          <div className="flex mt-6 gap-x-12">
-            {item?.colorList?.map((c, i) => (
-              <ColorIcon
-                key={i}
-                color={c}
-                current={currentColor.id === c.id}
-                colorOnClick={(c) => colorOnClick(c)}
-              />
-            ))}
-          </div>
-          <p className="leading-6 mt-7 capitalize">
-            {currentColor?.name.toLowerCase()}
-          </p>
-          <p className="leading-6 mt-9">{item?.price + "$"}</p>
-          <div className="flex mt-9 gap-x-[23px]">
-            {currentColor?.details.map((s, i) => {
-              return (
-                <div
-                  key={i}
-                  className={`w-[64px] h-[66px]  flex cursor-pointer ${
-                    currentSize === s.size ? "bg-primary" : "bg-[#D9D9D9] "
-                  }`}
-                  onClick={() => sizeOnClick(s.size)}
-                >
-                  <h3
-                    className={`text-[18px] leading-[42px] m-auto ${
-                      currentSize === s.size ? "text-white" : "text-nav-item "
+        {item ? (
+          <div className="flex basis-[47%] justify-between gap-x-7 items-start">
+            {/* Main Image */}
+            <img
+              src={currImage?.url}
+              alt="Main"
+              className="object-cover object-center basis-[85%] bg-gray-400"
+              style={{ height: "80vh" }}
+            />
+            {/* Sub Images */}
+            <div
+              className="grid grid-flow-row basis-[12.5%] gap-y-[10px] cursor-pointer overflow-y-auto"
+              style={{ height: currentColor?.imgList.length > 5 && "80vh" }}
+            >
+              {currentColor?.imgList.map((image, i) => {
+                return (
+                  <img
+                    key={i}
+                    src={image.url}
+                    alt="Product"
+                    className={`object-cover object-center aspect-[55/82] hover:border-2 hover:border-primary hover:border-solid ${
+                      currImage.id === i &&
+                      "border-2 border-primary border-solid"
                     }`}
-                  >
-                    {s.size}
-                  </h3>
-                </div>
-              );
-            })}
-          </div>
-          <div className="flex mt-16 max-h-[49px] gap-x-[82px]">
-            {/* Quantity */}
-            <Quantity custom="w-36" />
-            {/* Add To Cart */}
-            <div className="flex bg-[#EBF6FF] rounded-[5px] items-center px-[21px] gap-x-[15px] cursor-pointer">
-              <img src={CartIcon} alt="Cart" className="h-[17px] w-[17px]" />
-              <h3 className=" text-secondary">Add To Cart</h3>
+                    onClick={() => imageOnClick(i, image.url)}
+                  />
+                );
+              })}
             </div>
           </div>
+        ) : (
+          <Skeleton.Image
+            active
+            style={{ width: "100%", height: "80vh" }}
+            className="w-[47%]"
+          />
+        )}
+        {/* Right */}
+        <div className=" basis-[36.5%] mt-[82px]">
+          {item ? (
+            <div>
+              <h1 className=" font-bold">{item?.name}</h1>
+              <p className=" leading-6 mt-7">{item?.description}</p>
+              <div className="flex mt-6 gap-x-12">
+                {item?.colorList?.map((c, i) => (
+                  <ColorIcon
+                    key={i}
+                    color={c}
+                    current={currentColor.id === c.id}
+                    colorOnClick={(c) => colorOnClick(c)}
+                  />
+                ))}
+              </div>
+              <p className="leading-6 mt-7 capitalize">
+                {currentColor?.name.toLowerCase()}
+              </p>
+              <p className="leading-6 mt-9">{item?.price + "$"}</p>
+              <div className="flex mt-9 gap-x-[23px]">
+                {currentColor?.details.map((s, i) => {
+                  return (
+                    <div
+                      key={i}
+                      className={`w-[64px] h-[66px]  flex cursor-pointer ${
+                        currentSize === s.size
+                          ? "bg-primary"
+                          : "bg-[#D9D9D9] hover:border-2 hover:border-primary hover:border-solid"
+                      }`}
+                      onClick={() => sizeOnClick(s.size)}
+                    >
+                      <h3
+                        className={`text-[18px] leading-[42px] m-auto ${
+                          currentSize === s.size
+                            ? "text-white"
+                            : "text-nav-item "
+                        }`}
+                      >
+                        {s.size}
+                      </h3>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="flex mt-16 max-h-[49px] gap-x-[82px]">
+                {/* Quantity */}
+                <Quantity custom="w-36" />
+                {/* Add To Cart */}
+                <div className="flex bg-[#EBF6FF] rounded-[5px] items-center px-[21px] gap-x-[15px] cursor-pointer">
+                  <img
+                    src={CartIcon}
+                    alt="Cart"
+                    className="h-[17px] w-[17px]"
+                  />
+                  <h3 className=" text-secondary">Add To Cart</h3>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <Skeleton className="w-full" />
+          )}
         </div>
       </div>
       <hr className="black-line w-[50%] mt-[274px]" />
       {/* SIMILAR ITEMS */}
-      <SimilarItems id={item?.id}/>
+      <SimilarItems id={item?.id} />
     </div>
   );
 };
