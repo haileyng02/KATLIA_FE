7872b43b29af990ff2f307ffce4580c7feac8758
haileyng02 <haileynguyenss@gmail.com>
2022-12-18 20:49:59 +0700

    done order history

--- src/api/appApi.jsx
+++ src/api/appApi.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const appApi = axios.create({
-  baseURL: 'http://localhost:2041'
+  baseURL: 'http://localhost:2041' //https://katlia-server.onrender.com
 })
 
 export default appApi
